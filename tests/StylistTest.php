d55b163186056fd43c1db24dbf6e05602e3b9d38
Xing Li <msuli_1120@ymail.com>
2017-03-31 14:42:34 -0700

    add update function

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -139,6 +139,29 @@
       $this->assertEquals("male",$test_client->getGender());
     }
 
+    function testUpdateAge(){
+      $name = "Xing";
+      $test_stylist = new Stylist($name);
+      $test_stylist->save();
+      $stylist_id = $test_stylist->getId();
+
+      $client_name = "Eliot";
+      $client_gender = "male";
+      $client_age = 33;
+      $client_address = "downtown";
+      $test_client = new Client($client_name, $client_gender, $client_age, $client_address, $stylist_id);
+      $test_client->save();
+
+      $client_name2 = "Max";
+      $client_gender2 = "male";
+      $client_age2 = 25;
+      $client_address2 = "belletown";
+
+      $test_client->update($client_name2, $client_gender2, $client_age2, $client_address2);
+
+      $this->assertEquals(25,$test_client->getAge());
+    }
+
 
 
   }
